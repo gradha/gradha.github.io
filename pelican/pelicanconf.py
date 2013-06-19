be456cdcea06f829a72f268cbf677b1b1ddfc599
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2013-06-20 00:14:06 +0200

    Customizes footers and stuff.

--- pelican/pelicanconf.py
+++ pelican/pelicanconf.py
@@ -4,26 +4,29 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Grzegorz Adam Hankiewicz'
 SITENAME = u'Rants from the Ballmer Peak'
-SITEURL = ''
+SITEURL = 'http://gradha.github.io'
 
 TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
+FEED_DOMAIN = SITEURL
+FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
+FEED_MAX_ITEMS = 10
 
 # Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+LINKS =  (('Idiot Toys', 'http://idiottoys.com'),
+		('Kpop', 'http://www.reddit.com/r/kpop/'),
+		('Gon Lamperouge', 'http://gonlamperouge.tumblr.com'),
+		('FinoFIlipino', 'http://finofilipino.org'),
+		)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Nimrod', 'http://nimrod-code.org/news.xml'),
+		('Nimrod forum posts', 'http://forum.nimrod-code.org/postActivity.xml'),)
 
 DEFAULT_PAGINATION = 5
 
