0e27692b3b49f826debc20771b911a6ea0dff469
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2014-03-26 21:13:59 +0100

    Updates static files.

--- src/update_static_files.sh
+++ src/update_static_files.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-git commit -av -m "Updates static files." &&
+# Check that we are runnign at the correct directory level.
+if [ ! -d manual ]
+then
+	echo "Huh, no manual directory found?"
+    exit 1
+fi
+
+git add .. && git commit -av -m "Updates static files." &&
 	git show --stat
